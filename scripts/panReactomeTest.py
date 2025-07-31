ce0ebed18a9a5a2a55a70e9c57f506dbc8417a76
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2025-07-31 12:13:51 +0200

    minor edits to fix architecture

--- scripts/panReactomeTest.py
+++ scripts/panReactomeTest.py
@@ -4,33 +4,35 @@ Created on Wed Jul 30 17:55:28 2025
 
 @author: drgarza
 """
+import os
+import cobra
+from pathlib import Path
+from build_pan_reactomes import make_panReactome, add_exchange, generate_binary_presence_matrix
 
-from build_pan_reactomes import *
-
-model_folder = os.path.join(Path(os.getcwd()).parents[0], 
-                            'files', 
-                            'models', 
-                            'AGORA', 
-                            'pan_genome')
+root_dir = Path(__file__).resolve().parent.parent
+model_folder = root_dir / 'files' / 'models' / 'AGORA' / 'pan_genome'
 
 models = os.listdir(model_folder)
 
-model = cobra.io.read_sbml_model(os.path.join(model_folder, 'Bacteroides_thetaiotaomicron_VPI_5482.xml'))
+model = cobra.io.read_sbml_model(
+    os.path.join(model_folder, 'Bacteroides_thetaiotaomicron_VPI_5482.xml')
+)
 
 for reaction in model.reactions:
-    if reaction.objective_coefficient==1.0:
+    if reaction.objective_coefficient == 1.0:
         objective = reaction.copy()
 
 preact = make_panReactome(model_folder, 'Bacteroides_panreactome')
 
-#where we save the image and reaction presence absence table
-results_folder = os.path.join(Path(os.getcwd()).parents[0], 
-                              'files', 
-                              'results', 
-                              'pan_reactome')
-generate_binary_presence_matrix(model_folder, 
-                                preact, 
-                                results_folder)
+# Path where we save the image and reaction presence absence table
+results_folder = root_dir / 'results' / 'pan_reactome'
+os.makedirs(results_folder, exist_ok=True)
+
+generate_binary_presence_matrix(
+    model_folder,
+    preact,
+    results_folder
+)
 
 add_exchange(preact)
 preact.add_reactions([objective])
