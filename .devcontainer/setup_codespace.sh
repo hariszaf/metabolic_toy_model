93fada1b7c6b3bbe79981f7cd4bc244c6df01dc3
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2025-09-16 10:20:16 +0000

    install conda as vscode user - not sudo

--- .devcontainer/setup_codespace.sh
+++ .devcontainer/setup_codespace.sh
@@ -12,7 +12,7 @@ CONDA_BIN="$MINICONDA_DIR/bin/conda"
 
 if [ ! -d "$MINICONDA_DIR" ]; then
     wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
-    sudo bash /tmp/miniconda.sh -b -p "$MINICONDA_DIR"
+    bash /tmp/miniconda.sh -b -p "$MINICONDA_DIR"  # no sudo anymore
     rm /tmp/miniconda.sh
 fi
 
@@ -23,11 +23,17 @@ export PATH="$MINICONDA_DIR/bin:$PATH"
 # Initialize conda for bash (without sudo)
 eval "$($CONDA_BIN shell.bash hook)"
 
+
+# Accept conda TOS
+echo "Ensuring Anaconda TOS acceptance..."
+$CONDA_BIN tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+$CONDA_BIN tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
 # Force base to use Python 3.10
+echo "Having Python 3.10 in base environment..."
 $CONDA_BIN install -n base python=3.10 -y
 
 # Accept conda terms of service
-echo "Ensuring Anaconda TOS acceptance..."
 $CONDA_BIN config --system --set always_yes true
 $CONDA_BIN config --system --set auto_update_conda false
 
